a038ae60fa4b35d6c3ed6f81cc4de3ecb7a31deb
NiteshKumarMehra <102132087+NiteshKumarMehra@users.noreply.github.com>
2022-04-27 11:24:34 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 const {chromium} = require('playwright');
 const expect = require('expect');
 
-(async() => {
+async() => {
 const browser = await chromium.launch({headless:false, slowMo: 50})  
 const context = await browser.newContext()
 const page = await context.newPage()
